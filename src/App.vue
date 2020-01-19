392b61e1b174a4e98eb82594d2604f6869611550
Patrik <patriknilsson1992@gmail.com>
2020-01-19 14:18:22 +0700

    add starter code

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,12 @@
 <template>
-  <div id="app">
-    <credit-card-form />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import CreditCardForm from "./components/CreditCardForm";
-
 export default {
-  name: "app",
-  components: {
-    CreditCardForm
-  }
+  name: "app"
 };
 </script>
 
 <style lang="scss">
-#app {
-  @import url("https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700&display=swap");
-  @import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
-  font-family: "Roboto", sans-serif;
-  margin: 0 auto;
-  text-align: center;
-  color: #2c3e50;
-
-  * {
-    box-sizing: border-box;
-    &:focus {
-      outline: none;
-    }
-  }
-}
 </style>
